7b16e831bd61e8c791c7df6f152c9a8722cd0f37
Justin Banusing <45519338+popi1141@users.noreply.github.com>
2025-10-06 05:45:27 -0700

    Refactor app to Next.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { ThemeProvider } from '@material-ui/core';
 import {
   BrowserRouter,
---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+import '../index.css';
+
+function ClarityApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default ClarityApp;
